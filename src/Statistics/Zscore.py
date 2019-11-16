f2867695f868d2557b86a4f5594c62f76c5071fe
emeraldumstead1 <edu2@njit.edu>
2019-11-16 10:24:06 -0500

    Feature: Added Zscore

---
+++ src/Statistics/Zscore.py
@@ -0,0 +1,11 @@
+from src.Calculator.Addition import addition
+from src.Calculator.Division import division
+
+
+function zscore (
+		x        : numeric,
+		flag [1] : integer,
+		dims [*] : integer
+	)
+
+	return_val  :  float or double
