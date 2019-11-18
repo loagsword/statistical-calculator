27f4ca03d7e6b2a20e4342089dc8636057d2b49b
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-11-18 17:36:03 -0500

    Feature: Added method for finding sample standard deviation

--- src/Statistics/Getsample.py
+++ src/Statistics/Getsample.py
@@ -2,5 +2,9 @@ import random
 
 
 def get_sample(data, sample_size):
-    random_values = random.sample(data, k=sample_size)
-    return random_values
+    try:
+        random_values = random.sample(data, k=sample_size)
+        return random_values
+    except ValueError:
+        print("Value Error: Check your data inputs. "
+              "Sample is larger than population or may be negative")
