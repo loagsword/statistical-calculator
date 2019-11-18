7d576aad90aed413c6f8f2ad50f3383f6ce29a24
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-11-18 17:36:03 -0500

    Feature: Added method for finding sample mean

--- src/Statistics/Statistics.py
+++ src/Statistics/Statistics.py
@@ -1,5 +1,6 @@
 from src.Calculator.Calculator import Calculator
 from src.Statistics.Mean import mean
+from src.Statistics.SampleMean import sample_mean
 from src.CsvReader.csvReader import CsvReader
 from src.CsvReader.csvReader2 import csv_reader2
 
@@ -9,4 +10,8 @@ class Statistics(Calculator):
 
     def mean(self, data):
         self.result = mean(data)
+        return self.result
+
+    def sample_mean(self, data, sample_size):
+        self.result = sample_mean(data, sample_size)
         return self.result
