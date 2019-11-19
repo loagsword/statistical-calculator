9e958673a118e39ca20a1a5d88420b03e2f228ac
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-11-18 19:25:35 -0500

    Feature: finished method for finding sample standard deviation

--- src/Statistics/Statistics.py
+++ src/Statistics/Statistics.py
@@ -1,8 +1,7 @@
 from src.Calculator.Calculator import Calculator
 from src.Statistics.Mean import mean
 from src.Statistics.SampleMean import sample_mean
-from src.CsvReader.csvReader import CsvReader
-from src.CsvReader.csvReader2 import csv_reader2
+from src.Statistics.SampleStandardDeviation import sample_std_deviation
 
 
 class Statistics(Calculator):
@@ -14,4 +13,8 @@ class Statistics(Calculator):
 
     def sample_mean(self, data, sample_size):
         self.result = sample_mean(data, sample_size)
+        return self.result
+
+    def sample_std_deviation(self, sample):
+        self.result = sample_std_deviation(sample)
         return self.result
